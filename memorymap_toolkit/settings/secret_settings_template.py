ac95520aeb0d1c058af35329f5110a33aafb7eec
Leonardo Henzen <henzenleo03@gmail.com>
2026-05-21 18:39:20 +0200

    Fix ALLOWED_HOSTS handling (split env var)

--- memorymap_toolkit/settings/secret_settings_template.py
+++ memorymap_toolkit/settings/secret_settings_template.py
@@ -3,7 +3,7 @@ import os
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
-ALLOWED_HOSTS = [os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost')]
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').split()
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
