d50690c39afd99b170e12eba538c18dd1fe224c4
Leonardo Henzen <henzenleo03@gmail.com>
2026-05-22 17:44:44 +0200

    Add safe MapTiler URL handling with fallback

--- memorymap_toolkit/context_processors.py
+++ memorymap_toolkit/context_processors.py
@@ -4,6 +4,17 @@ from django.conf import settings
 # Third Party Django apps
 from constance import config
 
+try:
+    from .utils import ensure_maptiler_key
+except ImportError:
+    def ensure_maptiler_key(url, key):
+        return url
 
 def site_settings(request):
-    return {'config': config}
+    return {
+        'config': config,
+        'config_base_map_style_url': ensure_maptiler_key(
+            config.BASE_MAP_STYLE_URL,
+            getattr(config, "MAPTILER_KEY", None)
+        )
+    }
